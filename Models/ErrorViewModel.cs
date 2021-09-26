514791d51ddadae94ab8215d03e72023e748786c
Lucas Toshio Löschner Fujiwara <lucastfujiwara@gmail.com>
2021-09-26 11:31:53 -0300

    start

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MonitorPoliticoMAC413.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
