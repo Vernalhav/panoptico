bb66056e3247169ff4c90f93d81b97e4c577ea1f
Victor Giovannoni <vernalhav@usp.br>
2021-10-14 16:29:42 -0300

    [WIP] frontend project setup
    co-authored-by: Tiago <tmsilva11@usp.br>
    co-authored-by: Van Loon <gabriel.vl.rojas@gmail.com>

---
+++ political-tracker-frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button';
+import { Routes } from '@angular/router';
+import { HomePageComponent } from 'src/pages/home-page/home-page.component';
+
+const routes: Routes = [
+  {
+    component: HomePageComponent 
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
