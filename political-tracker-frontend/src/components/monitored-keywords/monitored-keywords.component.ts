0eeab4088dc2a52736056304f03ab8058b2cd3f5
Lucas Fujiwara <lucastfujiwara@gmail.com>
2021-10-30 17:04:26 -0300

    fix: remove unused import

--- political-tracker-frontend/src/components/monitored-keywords/monitored-keywords.component.ts
+++ political-tracker-frontend/src/components/monitored-keywords/monitored-keywords.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MonitoredKeywordsController } from 'src/core/controller/monitored-keywords.controller';
 import { MonitoredKeywordsView } from 'src/core/view/monitored-keywords.view';
 
