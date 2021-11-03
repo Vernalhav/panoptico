3403fe42d9a38a772749fafa840c06d9c80624ee
Victor Giovannoni <vernalhav@usp.br>
2021-11-03 18:22:44 -0300

    Add query builder query skeleton
    co-authored-by: Van Loon <gabriel.vl.rojas@gmail.com>
    co-authored-by: Tiago <tmsilva11@gmail.com>

--- political-tracker-backend/src/voting/voting.service.ts
+++ political-tracker-backend/src/voting/voting.service.ts
@@ -1,13 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { getConnection, Repository } from 'typeorm';
+import { getConnection, Between } from 'typeorm';
 import { Subject } from './entities/subject.interface'
 
 @Injectable()
 export class VotingService {
   constructor() {}
 
-  async getAll(partiesIds?: number[], congresspersonIds?: number[], subjects?: string[], regexSubjects?: string[], startDate?: string, endDate?: string){
-    const data = await getConnection().manager.query("SELECT * FROM votos LIMIT 30;");
+  async getAll(partiesIds?: number[],
+                congresspersonIds?: number[], 
+                subjects = ['Previdência e Assistência Social'],
+                regexSubjects = ['Fi.*', 'aaaaaa', 'P+'],
+                startDate = '2019-04-24',
+                endDate = '2019-07-10'){
+    
+    const query = await getConnection().manager.createQueryBuilder()
+                  .select(['v.idVotacao', 'pV.dataVotacao', 'COUNT(*) as total'])
+                  .addSelect(`'[' || group_concat(DISTINCT '"' || REPLACE(t.nome, '"', '') || '"') || ']'`, 'temas')
+                  .addSelect(`SUM(CASE WHEN (v.voto = 'Sim') THEN 1 ELSE 0 END)`, 'sim')
+                  .addSelect(`SUM(CASE WHEN (v.voto = 'Não') THEN 1 ELSE 0 END)`, 'nao')
+                  .addSelect(`SUM(CASE WHEN (v.voto = 'Abstenção') THEN 1 ELSE 0 END)`, 'abstencao')
+                  .addSelect(`SUM(CASE WHEN (v.voto IN ('Sim', 'Não', 'Abstenção')) THEN 0 ELSE 1 END)`, 'outros')
+                  .from('votos', 'v')
+                  .innerJoin('proposicoesVotacoes', 'pV', 'v.idVotacao = pV.idVotacao AND pV.dataVotacao BETWEEN :start AND :end',
+                    {
+                      'start': startDate,
+                      'end': endDate
+                    })
+                  .innerJoin('proposicoesTemas', 'pT', 'pV.idProposicao = pT.idProposicao')
+                  .innerJoin('topicos', 't', `pT.idTopico = t.id AND (t.nome IN (:...topics))`,
+                    {
+                      'topics': subjects,
+                    })
+                  .groupBy('v.idVotacao')
+                  .addGroupBy('pv.dataVotacao')
+                  .limit(5);
+    
+    console.log(query.getSql());
+    const data = await query.getRawMany();
+    data.forEach(element => {
+      element.temas = JSON.parse(element.temas)
+    });;
 
     // SELECT v.idVotacao,
     //    pV.dataVotacao,
