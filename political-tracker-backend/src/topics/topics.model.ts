2d69adb396af9a64a4b81df717b2238aba69c366
Victor Giovannoni <vernalhav@usp.br>
2021-11-03 18:23:22 -0300

    Add topic model interface specification

---
+++ political-tracker-backend/src/topics/topics.model.ts
@@ -0,0 +1,6 @@
+// Carrega lista de todos os tópicos do BD
+// Filtra essa lista (na RAM) fazendo um array.filter()
+//    com a condição de filtro sendo o match em algum
+//    dos patterns
+// retornar lista
+// getTopicsByRegexList(regexList: string[]): string[]
