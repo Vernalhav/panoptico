fbde339f6e974f86f3203fc5c4ec3fd7bd624b6f
Tiago <tmsilva11@usp.br>
2021-12-03 20:05:03 -0300

    update tests

--- backend/src/tests/services/subject.service.spec.ts
+++ backend/src/tests/services/subject.service.spec.ts
@@ -1,96 +1,112 @@
-// ============= TODO GIGANTE AQUI VÉI ===================
+import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { SubjectService } from 'src/services';
+import { Subject } from 'src/entities';
 
-// import { SubjectService } from 'src/services/subject.service';
-
-// describe('TopicService', () => {
-//   let topicService: SubjectService;
-//   let allSubjects: string[];
+describe('Subject Service', () => {
+  let service: SubjectService;
+  const allSubjects: Subject[] = 
+  [
+    {id: 0, votings: [], name: 'Relações Internacionais e Comércio Exterior'},
+    {id: 1, votings: [], name: 'Saúde'},
+    {id: 2, votings: [], name: 'Defesa e Segurança'},
+    {id: 3, votings: [], name: 'Trabalho e Emprego'},
+    {id: 4, votings: [], name: 'Turismo'},
+  ];
   
-//   beforeEach(() => {
-//     topicService = new SubjectService();
-//     allSubjects = [
-//       'Relações Internacionais e Comércio Exterior', 'Saúde', 
-//       'Defesa e Segurança', 'Trabalho e Emprego', 
-//       'Turismo'
-//     ].sort();
+  const mockedRepo = {
+    find: jest.fn((id) => Promise.resolve(allSubjects)),
+  }
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        SubjectService,
+        // Mock repository
+        {
+          provide: getRepositoryToken(Subject),
+          useValue: mockedRepo,
+        }
+      ]
+    }).compile();
     
-//     jest.spyOn(topicService, 'getAll').mockImplementation(async () => Promise.resolve(allSubjects));
-//   });
+    service = await module.get(SubjectService);
+  });
 
-//   describe('getByRegexList', () => {
-//     it('should return all subjects', async () => {
-//       const regExArray = ['.*'];
+  describe('getByRegexList', () => {
+    it('should return all subjects', async () => {
+      const regExArray = ['.*'];
       
-//       const expected = allSubjects;
-//       const received = (await topicService.getByRegexList(regExArray)).sort();
+      const expected = allSubjects;
+      const received = (await service.getFilteredByName([], regExArray)).sort();
       
-//       expect(received).toEqual(expected);
-//     });
-//   });
+      expect(received).toEqual(expected);
+    });
+  });
 
-//   describe('getByRegexList', () => {
-//     it('should return all subjects starting with T', async () => {
-//       const regExArray = ['^T'];
+  describe('getByRegexList', () => {
+    it('should return all subjects starting with T', async () => {
+      const regExArray = ['^T'];
       
-//       const expected = ['Trabalho e Emprego', 'Turismo'].sort();
-//       const received = (await topicService.getByRegexList(regExArray)).sort();
+      const expected = ['Trabalho e Emprego', 'Turismo'].sort();
+      const received = (await service.getFilteredByName([], regExArray)).sort();
       
-//       expect(received).toEqual(expected);
-//     });
-//   });
+      expect(received).toEqual(expected);
+    });
+  });
 
-//   describe('getByRegexList', () => {
-//     it('should return all subjects starting with T or S', async () => {
-//       const regExArray = ['^T', '^S'];
+  describe('getByRegexList', () => {
+    it('should return all subjects starting with T or S', async () => {
+      const regExArray = ['^T', '^S'];
       
-//       const expected = ['Trabalho e Emprego', 'Turismo', 'Saúde'].sort();
-//       const received = (await topicService.getByRegexList(regExArray)).sort();
+      const expected = ['Trabalho e Emprego', 'Turismo', 'Saúde'].sort();
+      const received = (await service.getFilteredByName([], regExArray)).sort();
       
-//       expect(received).toEqual(expected);
-//     });
-//   });
+      expect(received).toEqual(expected);
+    });
+  });
 
-//   describe('getByRegexList', () => {
-//     it('should return an empty array when filtering with invalid RegEx only', async () => {
-//       const regExArray = ['*'];
+  describe('getByRegexList', () => {
+    it('should return an empty array when filtering with invalid RegEx only', async () => {
+      const regExArray = ['*'];
       
-//       const expected = [];
-//       const received = await topicService.getByRegexList(regExArray);
+      const expected = [];
+      const received = (await service.getFilteredByName([], regExArray)).sort();
       
-//       expect(received).toEqual(expected);
-//     });
-//   });
+      expect(received).toEqual(expected);
+    });
+  });
 
-//   describe('getByRegexList', () => {
-//     it('should return an empty array', async () => {
-//       const regExArray = ['Política.*', 'Educação.*', '^K'];
+  describe('getByRegexList', () => {
+    it('should return an empty array', async () => {
+      const regExArray = ['Política.*', 'Educação.*', '^K'];
       
-//       const expected = [];
-//       const received = await topicService.getByRegexList(regExArray);
+      const expected = [];
+      const received = (await service.getFilteredByName([], regExArray)).sort();
       
-//       expect(received).toEqual(expected);
-//     });
-//   });
+      expect(received).toEqual(expected);
+    });
+  });
 
-//   describe('getByRegexList', () => {
-//     it('should return an array with subjects starting with T and ignore the invalid RegEx', async () => {
-//       const regExArray = ['*', '^T'];
+  describe('getByRegexList', () => {
+    it('should return an array with subjects starting with T and ignore the invalid RegEx', async () => {
+      const regExArray = ['*', '^T'];
       
-//       const expected = ['Trabalho e Emprego', 'Turismo'].sort();
-//       const received = (await topicService.getByRegexList(regExArray)).sort();
+      const expected = ['Trabalho e Emprego', 'Turismo'].sort();
+      const received = (await service.getFilteredByName([], regExArray)).sort();
       
-//       expect(received).toEqual(expected);
-//     });
-//   });
+      expect(received).toEqual(expected);
+    });
+  });
 
-//   describe('getByRegexList', () => {
-//     it('should return an empty array', async () => {
-//       const regExArray = [];
+  describe('getByRegexList', () => {
+    it('should return an empty array', async () => {
+      const regExArray = [];
       
-//       const expected = [];
-//       const received = await topicService.getByRegexList(regExArray);
+      const expected = [];
+      const received = (await service.getFilteredByName([], regExArray)).sort();
       
-//       expect(received).toEqual(expected);
-//     });
-//   });
-// });
+      expect(received).toEqual(expected);
+    });
+  });
+});
