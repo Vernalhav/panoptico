67ce639e643e59e5e5e3d3d0866f5bfa5762a404
Victor Giovannoni <vernalhav@usp.br>
2021-12-17 19:54:36 -0300

    delete old homepage

--- frontend/src/app/pages/pages.module.ts
+++ frontend/src/app/pages/pages.module.ts
@@ -22,7 +22,6 @@ import { CoreModule } from '../core/core.module';
 import { ComponentsModule } from '../components/components.module';
 import { SharedModule } from '../shared/shared.module';
 
-import { HomePageComponent } from './home-page/home-page.component';
 import { VotingsPageComponent } from './votings-page/votings-page.component';
 import { TabCloudsComponent } from './mon-ent-votes-subjects/tab-clouds/tab-clouds.component';
 import { HomeComponent } from './home/home.component';
@@ -37,7 +36,6 @@ import { MonEntVotesSubjectsComponent } from './mon-ent-votes-subjects/mon-ent-v
 @NgModule({
   declarations: [
     // Pages
-    HomePageComponent, 
     VotingsPageComponent, 
     
     // Pages Components
